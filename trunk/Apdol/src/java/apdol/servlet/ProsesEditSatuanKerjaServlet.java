c80a5a819484f9ec7dddf2fbe6e366d02b9286cf
hareez.rz@gmail.com <hareez.rz@gmail.com@87066fb6-fcfc-bf47-7d67-4ec7d39fd532>
2011-12-22 05:11:07 +0000

    issue 46 : ProsesRekamSatuanKerjaServlet.java --> fix rainy day case code (dropdown error)
    issue 47 :
    ProsesEditSatuanKerjaServlet.java -->  fix rainy day case code (dropdown error)

--- trunk/Apdol/src/java/apdol/servlet/ProsesEditSatuanKerjaServlet.java
+++ trunk/Apdol/src/java/apdol/servlet/ProsesEditSatuanKerjaServlet.java
@@ -63,51 +63,84 @@ public class ProsesEditSatuanKerjaServlet extends HttpServlet {
             //validate blank field
             if (kodeSatker.equals("")) {
                 JOptionPane.showMessageDialog(null, "Kode Satker tidak boleh kosong !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } else if (namaSatker.equals("")) {
                 JOptionPane.showMessageDialog(null, "Nama Satker tidak boleh kosong !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } else if (kodeMenteri.equals("")) {
                 JOptionPane.showMessageDialog(null, "Kode Kementerian tidak boleh kosong !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } else if (namaMenteri.equals("")) {
                 JOptionPane.showMessageDialog(null, "Nama Kementerian tidak boleh kosong !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } else if (kodeUnit.equals("")) {
                 JOptionPane.showMessageDialog(null, "Kode Kementerian tidak boleh kosong !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } else if (namaUnit.equals("")) {
                 JOptionPane.showMessageDialog(null, "Nama Kementerian tidak boleh kosong !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } //validate length field
             else if (kodeSatker.length() < 6) {
                 JOptionPane.showMessageDialog(null, "Kode Satker harus 6 angka !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } //validate kodeSatker are numbers
             else if (!this.valNumber(kodeSatker)) {
                 JOptionPane.showMessageDialog(null, "Kode Satker harus angka dan tidak boleh minus !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } //validate zero value
             else if (kodeSatker.equalsIgnoreCase("000000")) {
                 JOptionPane.showMessageDialog(null, "Kode Satker tidak boleh bernilai nol !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } //validate record on database
             else if (daftarSatker.isKodeExist(kodeSatker) && !satker.isKodeNoChange(kodeSatker)) {
                 JOptionPane.showMessageDialog(null, "Kode Satker sudah ada dalam database !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 //response.sendRedirect("rekam_satker");
                 jsp = "pages/edit_satker.jsp";
             } //validate record on database
             else if (daftarSatker.isNamaSatkerExist(namaSatker) && !satker.isNamaSatkerNoChange(namaSatker)) {
                 JOptionPane.showMessageDialog(null, "Nama Satker sudah ada dalam database !");
+                List<Lokasi> listNamaLokasi = daftarLokasi.getLokasi();
+                request.setAttribute("list_lokasi", listNamaLokasi);
+                request.setAttribute("satker_edit", satker);
                 jsp = "pages/edit_satker.jsp";
             } else {
                 satker.setKodeSatker(kodeSatker);
@@ -125,9 +158,6 @@ public class ProsesEditSatuanKerjaServlet extends HttpServlet {
                 //response.sendRedirect("satker");
             }
 
-            request.setAttribute("satker_edit", satker);
-            request.setAttribute("lokasiSatker", satker.getLokasi());
-            request.setAttribute("list_lokasi", listLokasi);
             RequestDispatcher requestDispatcher = request.getRequestDispatcher(jsp);
             requestDispatcher.forward(request, response);
         } finally {
